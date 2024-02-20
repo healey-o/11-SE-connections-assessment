1a50b15426172e1e2aebb506d5a32415daa66ad8
Oliver Healey <ohealey@student.saintaug.nsw.edu.au>
2024-02-20 18:50:19 +1100

    Defined the main game functions

---
+++ main.py
@@ -0,0 +1,32 @@
+
+#Selects four random categories of words
+def get_random_categories():
+    pass
+
+#Takes the four categories and randomly places the words in a 4x4 grid
+def randomize_grid(categories):
+    pass
+
+#Main game loop
+def game_loop(categories, grid):
+    pass
+
+#Displays the grid and associated info to the player
+def display_grid(grid, lives, found_categories, categories):
+    pass
+
+#Prompts the player for their four guesses
+def get_guesses():
+    pass
+
+#Checks the player's guesses against the four categories
+def check_guesses(guesses, categories, found_categories):
+    pass
+
+#Checks if all categories have been found
+def check_win(found_categories):
+    pass
+
+#Asks the player if they wish to play again or exit
+def prompt_play_again():
+    pass
